6beec1832824b7058c1744b29df45be741c8c3b8
abirhasanbs23 <abir.hasan@brainstation-23.com>
2024-02-01 02:22:30 +0600

    project setup

---
+++ src/main/java/com/example/ASYNDM/ASYNDM/AsyndmApplication.java
@@ -0,0 +1,13 @@
+package com.example.ASYNDM.ASYNDM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyndmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyndmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ASYNDM/ASYNDM/AsyndmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ASYNDM.ASYNDM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyndmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
